e1608923e7e138f57fbb5b250f078afd7913704c
siddshrestha <45787147+siddshrestha@users.noreply.github.com>
2021-03-15 14:35:41 +0545

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <add-blog></add-blog>
+  </div>
+</template>
+
+<script>
+import addBlog from './components/addBlog.vue';
+export default {
+  components: {
+    'add-blog': addBlog
+  },
+  data () {
+    
+  }
+}
+</script>
+
+<style>
+body{
+  margin: 0;
+  font-family: 'Nunito SemiBold';
+}
+</style>
