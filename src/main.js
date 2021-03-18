14ba4a4102a0574c331f4f87f5078615087ed598
siddshrestha <45787147+siddshrestha@users.noreply.github.com>
2021-03-18 17:37:11 +0545

    registering things locally

--- src/main.js
+++ src/main.js
@@ -4,21 +4,13 @@ import VueResource from 'vue-resource'
 
 Vue.use(VueResource);
 
-//filters
-Vue.filter('to-uppercase', function(value){
-  return value.toUpperCase();
-});
+
 
 Vue.filter('snippet', function(value){
   return value.slice(0, 100) + '...';
 })
 
-//custom directives
-Vue.directive('rainbow',{
-  bind(el, binding, vnode){
-    el.style.color = "#" + Math.random().toString().slice(2,8);
-  }
-});
+
 
 Vue.directive('theme', {
   bind(el, binding, vnode){
