b49e4b94d69bdef911ad4d0a194c89d06146ba45
siddshrestha <45787147+siddshrestha@users.noreply.github.com>
2021-03-21 20:53:39 +0545

    firebase

--- src/components/showBlogs.vue
+++ src/components/showBlogs.vue
@@ -3,8 +3,8 @@
     <h1>All blog Articles</h1>
     <input type="text" v-model="search" placeholder="search blogs">
     <div v-for="blog in filteredBlogs" class="single-blog">
-        <router-link v-bind:to="/blog/ + blog.id"><h2 v-rainbow>{{ blog.title | to-uppercase }}</h2></router-link><h2 v-rainbow>{{ blog.title | to-uppercase }}</h2>
-        <article>{{ blog.body| snippet  }}</article>
+        <router-link v-bind:to="/blog/ + blog.id"><h2>{{ blog.title | to-uppercase }}</h2></router-link>
+        <article>{{ blog.content| snippet  }}</article>
     </div>
   </div>
 </template>
@@ -27,9 +27,17 @@ export default {
       
   },
   created() {
-      this.$http.get('https://jsonplaceholder.typicode.com/posts').then(function(data){
+      this.$http.get('https://vuejsdummyblog-default-rtdb.firebaseio.com/posts.json').then(function(data){
 
-          this.blogs = data.body.slice(0,10);
+          return data.json();
+      }).then(function(data){
+        var blogsArray = [];
+        for(let key in data){
+          data[key].id = key
+          blogsArray.push(data[key]);
+        }
+        this.blogs = blogsArray;
+
       })
   },
   computed: {
